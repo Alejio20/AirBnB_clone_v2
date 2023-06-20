991d8859afdd66352b53495fdbe7e884c915c2e3
Alejio20 <humbleotamiri@gmail.com>
2023-06-20 14:00:41 -0700

    AirBnB

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -10,7 +10,38 @@ from os import path, remove
 import unittest
 from unittest.mock import patch
 from time import sleep
+import os
 
+
+class test_review(test_basemodel):
+    """ review test class"""
+
+    def __init__(self, *args, **kwargs):
+        """ review class init"""
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
+
+    def test_place_id(self):
+        """ testing review place_id attr"""
+        new = self.value()
+        self.assertEqual(type(new.place_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_user_id(self):
+        """ testing review user_id attr"""
+        new = self.value()
+        self.assertEqual(type(new.user_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
+
+    def test_text(self):
+        """ testing review text attr"""
+        new = self.value()
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
 class Test_instanceReview(unittest.TestCase):
 
